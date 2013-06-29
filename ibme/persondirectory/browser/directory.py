48594cf881b5cfef0892c8e6133e9f2280ad1989
Jamie Lentin <jm@lentin.co.uk>
2013-06-29 16:42:34 +0100

    Add some useful views

--- ibme/persondirectory/browser/directory.py
+++ ibme/persondirectory/browser/directory.py
@@ -2,6 +2,12 @@ from Products.Five import BrowserView
 
 class DirectoryView(BrowserView):
     """Class for all directory views"""
+    def personListing(self):
+        """Get all person objects for this view"""
+        listing = self.context.restrictedTraverse('@@folderListing')(**dict(
+            sort_order='descending',
+        ))
+        return [item for item in listing if item.isVisibleInNav()]
 
 class UniqueEntriesView(BrowserView):
     """Get unique entries for the specified field"""
