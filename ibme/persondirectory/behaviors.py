8e98bec0a5f1c2107e6d2e941017b47e98a65baa
Jamie Lentin <jm@lentin.co.uk>
2013-06-29 20:52:27 +0100

    Allow use of arbitary columns as filters

--- ibme/persondirectory/behaviors.py
+++ ibme/persondirectory/behaviors.py
@@ -1,12 +1,48 @@
+from zope import schema
+from zope.component import getUtility
 from zope.interface import implements, alsoProvides
+from zope.schema.interfaces import IVocabularyFactory
+from zope.schema.vocabulary import SimpleVocabulary
 
-from plone.autoform import directives, interfaces
+from z3c.form.browser.checkbox import CheckBoxFieldWidget
+
+from plone.autoform import interfaces
+from plone.dexterity.interfaces import IDexterityFTI
+from plone.directives import form
 from plone.supermodel import model
+
+from ibme.persondirectory import _
+
 
 class IPersonDirectory(model.Schema):
     """The top of a QnA form
     """
+    form.widget(filter_fields=CheckBoxFieldWidget)
+    filter_fields = schema.List(
+        title=_(u'Indexed person fields to filter'),
+        description=_(u'Select which fields have a catalog FieldIndex set-up'),
+        value_type=schema.Choice(
+            vocabulary='ibme.persondirectory.personfieldsvocab'),
+        default=[],
+        required=False)
+
 alsoProvides(IPersonDirectory, interfaces.IFormFieldProvider)
+
+
+class PersonFieldsVocab(object):
+    implements(IVocabularyFactory)
+    """Make a vocabulary of all pdir_person fields
+    """
+
+    def __new__(self, context):
+        """Fetch pdir_person's schema, and turn fields into vocab"""
+        schema = getUtility(IDexterityFTI, name='pdir_person').lookupSchema()
+
+        return SimpleVocabulary([
+            SimpleVocabulary.createTerm(name, name, schema[name].title)
+            for name in schema
+        ])
+
 
 class IPerson(model.Schema):
     """The top of a QnA form
--- ibme/persondirectory/browser/directory.py
+++ ibme/persondirectory/browser/directory.py
@@ -1,13 +1,49 @@
+from urllib import urlencode
+
+from zope.component import getUtility
+from zope.schema.interfaces import IVocabularyFactory
+
 from Products.Five import BrowserView
+
 
 class DirectoryView(BrowserView):
     """Class for all directory views"""
     def personListing(self):
         """Get all person objects for this view"""
-        listing = self.context.restrictedTraverse('@@folderListing')(**dict(
-            sort_order='descending',
-        ))
-        return [item for item in listing if item.isVisibleInNav()]
+        query = dict(sort_order='descending')
+
+        # Add any filters specified on the querystring
+        for f in self.context.filter_fields:
+            if f in self.request:
+                query[f] = self.request[f]
+
+        return self.context.restrictedTraverse('@@folderListing')(**query)
+
+    def uniqueFilterEntries(self):
+        """Return a dict of unique entries for each filter"""
+        out = dict()
+        portal_catalog = self.context.portal_catalog
+
+        for f in self.context.filter_fields:
+            out[f] = portal_catalog.Indexes[f].uniqueValues()
+        return out
+
+    def getPersonFieldTitle(self, id):
+        """Return the person field's title"""
+        return self.getPersonFieldVocab().getTerm(id).title
+
+    def getPersonFieldVocab(self):
+        """Return the person field name/title vocabulary"""
+        if getattr(self, 'fieldVocab', None) is None:
+            factory = getUtility(IVocabularyFactory,
+                                 'ibme.persondirectory.personfieldsvocab')
+            self.fieldVocab = factory(self.context)
+        return self.fieldVocab
+
+    def generateFilterUrl(self, filter, value):
+        """Return a URL with filter params added"""
+        return self.context.absolute_url() + '?' + urlencode({filter: value})
+
 
 class UniqueEntriesView(BrowserView):
     """Get unique entries for the specified field"""
