041519ca95f25d5a11e310f0fa65795e3c2c008e
Jamie Lentin <jm@lentin.co.uk>
2013-07-02 12:09:56 +0100

    Abstract all catalog-dealing functions into one place

--- ibme/persondirectory/browser/directory.py
+++ ibme/persondirectory/browser/directory.py
@@ -1,26 +1,25 @@
 from urllib import urlencode
 
-from zope.component import getUtility
-
-from plone.dexterity.interfaces import IDexterityFTI
-
 from Products.Five import BrowserView
 
-from ibme.persondirectory.catalog import uniqueValues
-
-WIDGET_NAME = 'ibme.persondirectory.widget.SuggestionFieldWidget'
+from ibme.persondirectory.catalog import uniqueValues, fieldToFilter, \
+    getFilterFields
 
 
 class DirectoryView(BrowserView):
     """Class for all directory views"""
     def personListing(self):
         """Get all person objects for this view"""
-        query = dict(portal_type='pdir_person')
+        query = dict(
+            sort_on='sortable_title',
+            sort_order='ascending',
+            portal_type='pdir_person')
 
         # Add any filters specified on the querystring
-        for (name, title) in self.getFilterFields():
-            if name in self.request:
-                query[name] = self.request[name]
+        query.update(fieldToFilter(dict(
+            (n, self.request[n])
+            for (n, t) in self.getFilterFields()
+            if n in self.request)))
 
         return self.context.restrictedTraverse('@@folderListing')(**query)
 
@@ -43,12 +42,7 @@ class DirectoryView(BrowserView):
     def getFilterFields(self):
         """Get all fields (and titles) that use the SuggestionFieldWidget"""
         if getattr(self, 'filter_fields', None) is None:
-            fti = getUtility(IDexterityFTI, name='pdir_person')
-            schema = fti.lookupSchema()
-            tags = schema.getTaggedValue(u'plone.autoform.widgets')
-            self.filter_fields = [(k, schema[k].title) for (k, v)
-                                  in tags.items()
-                                  if v == WIDGET_NAME]
+            self.filter_fields = getFilterFields()
         return self.filter_fields
 
     def generateFilterUrl(self, filter, value):
--- ibme/persondirectory/catalog.py
+++ ibme/persondirectory/catalog.py
@@ -1,3 +1,10 @@
+from zope.component import getUtility
+
+from plone.dexterity.interfaces import IDexterityFTI
+
+WIDGET_NAME = 'ibme.persondirectory.widget.SuggestionFieldWidget'
+
+
 def uniqueValues(portal_catalog, index):
     """Return the unique values for an index, creating it if necessary"""
     # If the index doesn't exist, create it and return nothing, as there can't
@@ -8,3 +15,26 @@ def uniqueValues(portal_catalog, index):
 
     # Return all non-empty items
     return [i for i in portal_catalog.Indexes[index].uniqueValues() if i]
+
+
+def fieldToFilter(fields):
+    """Turn field request into a filter"""
+    return fields
+
+
+def getFilterFields():
+    """Fetch all fields that use SuggestionFieldWidget"""
+    fti = getUtility(IDexterityFTI, name='pdir_person')
+    schema = fti.lookupSchema()
+    tags = schema.getTaggedValue(u'plone.autoform.widgets')
+
+    out = []
+    for (k, v) in tags.items():
+        if hasattr(v, 'getWidgetFactoryName'):
+            # Widget is wrapped by ParameterizedWidget
+            name = v.getWidgetFactoryName()
+        else:
+            name = v
+        if name == WIDGET_NAME:
+            out.append((k, schema[k].title,))
+    return out
