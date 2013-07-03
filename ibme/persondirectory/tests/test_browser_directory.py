1fc5567688ec6c157074a904067afe312d4beb26
Jamie Lentin <jm@lentin.co.uk>
2013-07-03 13:14:31 +0100

    Tests for view

---
+++ ibme/persondirectory/tests/test_browser_directory.py
@@ -0,0 +1,117 @@
+from plone.app.testing import setRoles, login, TEST_USER_NAME, TEST_USER_ID
+
+from Products.CMFCore.utils import getToolByName
+
+from .base import IntegrationTestCase
+
+
+class ContentTypeTest(IntegrationTestCase):
+
+    def setUp(self):
+        portal = self.layer['portal']
+        setRoles(portal, TEST_USER_ID, ['Manager'])
+        login(portal, TEST_USER_NAME)
+
+        self.setFullFatEntrySchema()
+        portal.invokeFactory(
+            type_name="pdir_directory",
+            id="dir",
+            title="UT Directory")
+        portal['dir'].invokeFactory(
+            type_name="pdir_entry",
+            id="ent1",
+            title=u'Damion Canterbruy-Zephraiah')
+        portal['dir'].invokeFactory(
+            type_name="pdir_entry",
+            id="ent2",
+            title=u'Alfred Dimpleby Zachery')
+        self.view = portal.unrestrictedTraverse('dir/view')
+
+    def fillFacetFields(self):
+        """Fill facet fields with some data"""
+        portal = self.layer['portal']
+
+        portal['dir']['ent1'].position = "Desk"
+        portal['dir']['ent1'].research_group = "Cat pictures"
+        portal['dir']['ent1'].reindexObject()
+        portal['dir']['ent2'].position = "Desk"
+        portal['dir']['ent2'].research_group = "Twitter"
+        portal['dir']['ent2'].reindexObject()
+
+    def test_personListing(self):
+        """Main content listings"""
+        portal = self.layer['portal']
+        catalog = getToolByName(portal, 'portal_catalog')
+        catalog.addIndex('pdir_keywords', 'KeywordIndex')
+        self.fillFacetFields()
+
+        # Show everything by default
+        self.assertEquals(
+            [x.id for x in self.view.personListing()],
+            ['ent2', 'ent1'])
+
+        # Sorting option is honoured
+        portal = self.layer['portal']
+        portal['dir'].sorting = "surname"
+        portal['dir']['ent1'].reindexObject()
+        portal['dir']['ent2'].reindexObject()
+        self.assertEquals(
+            [x.id for x in self.view.personListing()],
+            ['ent1', 'ent2'])
+
+        # Can filter too
+        request = self.layer['request']
+        request.set('research_group', 'Cat pictures')
+        self.assertEquals(
+            [x.id for x in self.view.personListing()],
+            ['ent1'])
+
+    def test_getTitle(self):
+        """Uses the facets specified in the querystring"""
+        self.assertEquals(
+            self.view.getTitle(),
+            "UT Directory")
+
+        request = self.layer['request']
+        request.set('rowsooch_gerp', 'not a filter')
+        request.set('position', 'Desk')
+        self.assertEquals(
+            self.view.getTitle(),
+            "UT Directory: Desk")
+
+    def test_getFacets(self):
+        """Get the facets specified in the querystring"""
+        request = self.layer['request']
+        request.set('rowsooch_gerp', 'not a filter')
+        request.set('position', 'Desk')
+        self.assertEquals(
+            self.view.getFacets(),
+            dict(position="Desk"))
+
+    def test_uniqueFilterEntries(self):
+        """Used to populate facets"""
+        self.assertEquals(
+            self.view.uniqueFilterEntries(),
+            {'position': [], 'research_group': []})
+
+        self.fillFacetFields()
+        self.assertEquals(
+            self.view.uniqueFilterEntries(), dict(
+                position=["Desk"],
+                research_group=["Cat pictures", "Twitter"],
+            ))
+
+    def test_getPersonFieldTitle(self):
+        """Get titles from field"""
+        self.assertEquals(
+            self.view.getPersonFieldTitle('position'),
+            u'Position')
+        self.assertEquals(
+            self.view.getPersonFieldTitle('research_group'),
+            u'Research Group')
+
+    def test_generateFilterUrl(self):
+        """Parameters should be encoded"""
+        self.assertEquals(
+            self.view.generateFilterUrl('camel', '2 humps please'),
+            'http://nohost/plone/dir?camel=2+humps+please')
